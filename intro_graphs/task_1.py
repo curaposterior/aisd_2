7ace79446587c6040a1dd960145a08234ae3b940
curaposterior <empo123@interia.pl>
2022-10-11 17:33:34 +0200

    adding dsf

--- intro_graphs/task_1.py
+++ intro_graphs/task_1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-from typing import Dict, List
 
-def wczytajListeSasiedztwa() -> Dict[int, List[int]]:
+def wczytajListeSasiedztwa() -> Dict[int, list[int]]:
     wierz = int(input("Wczytaj wierzcholki: "))
     kraw =  int(input("Wczytaj liczbe krawedzi: "))
     graph = {key: [] for key in range(wierz)}
@@ -15,13 +14,28 @@ def wczytajListeSasiedztwa() -> Dict[int, List[int]]:
 
     return  graph
 
-def wypiszGraf(graph: Dict[int, List[int]]) -> None:
+def wypiszGraf(graph: dict[int, list[int]]) -> None:
     print("Reprezentacja grafu:")
     for key,value in graph.items():
         if (len(value) != 0):
             print(str(key) + ".", value)
 
+#create dsf algo to search if graph has a path from a to b
+def dfs(graph: dict[int, list[int]], x: int, target: int) -> bool:
+    visited = set()
+    stack = [graph[x]]
+    while (len(stack) != 0):
+        node = stack[-1]
+        if (node == target):
+            return True
+
+        if node not in visited:
+            visited.add(node)
+            for nb in graph[node]:
+                stack.append(nb)        
+
 if __name__ == '__main__':
     graph = wczytajListeSasiedztwa()
     wypiszGraf(graph)
+
     print("It works...")
