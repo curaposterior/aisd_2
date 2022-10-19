8ab725d5f7f05d0af265d89d673c84950155e938
curaposterior <empo123@interia.pl>
2022-10-19 21:27:43 +0200

    adding bfs, dfs and adjList

---
+++ traversing_graphs/search_graph.py
@@ -0,0 +1,63 @@
+from queue import Queue
+
+
+class Graph:
+
+    def __init__(self, V):
+        self.l_wierzch = V
+        self.adjList = {key: [] for key in range(self.l_wierzch)}
+    
+    def addNode(self, u, v):
+        self.adjList[u].append(v)
+
+    def printGraph(self):
+        for key in self.adjList.keys():
+            print(str(key) + ":", self.adjList[key])
+    
+    def dfs(self, start):
+        print("\nDFS:")
+        visited = [False for i in range(self.l_wierzch)]
+        stack = []
+        stack.append(start)
+        while (len(stack)):
+            s = stack[-1]
+            stack.pop()
+            if (not visited[s]):
+                print(s, end=" ")
+                visited[s] = True
+            for n in self.adjList[s]:
+                if (not visited[n]):
+                    stack.append(n)
+
+    def bfs(self, start):
+        print("\nBFS")
+        
+        q = Queue()
+        visited = [False for i in range(self.l_wierzch)]
+        q.put(start)
+        visited[start] = True
+
+        while (not q.empty()):
+            node = q.get()
+            print(node, end=" ")
+            for uv in self.adjList[node]:
+                if visited[uv] == False:
+                    q.put(uv)
+                    visited[uv] = True
+
+
+if __name__ == '__main__':
+    grafik = Graph(8)
+
+    grafik.addNode(0,1)
+    grafik.addNode(1,2)
+    grafik.addNode(1,3)
+    grafik.addNode(2,4)
+    grafik.addNode(2,5)
+    grafik.addNode(3,6)
+    grafik.addNode(3,7)
+    
+    grafik.printGraph()
+    grafik.dfs(0)
+    grafik.bfs(0)
+    
